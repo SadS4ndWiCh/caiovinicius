320a7a6b978430779b630f3d66c6249a3a0c4182
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-01-13 14:41:41 -0300

    add projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,40 @@
+import type { GetStaticProps, NextPage } from "next";
+
+import { getAllProjects, IProject } from "@lib/graphcms";
+
+import { Layout } from "@components/Layouts/Layout";
+import { ProjectCard } from "@components/ProjectCard";
+
+import styles from '@styles/pages/Projects.module.scss';
+
+interface ProjectsProps {
+  allProjects: IProject[],
+};
+
+const Projects: NextPage<ProjectsProps> = ({ allProjects }) => {
+  return (
+    <Layout className={styles.container}>
+      <h1>All Projects</h1>
+
+      <ul className={styles.projectsList}>
+        { allProjects.map(project => (
+          <li key={project.slug}>
+            <ProjectCard project={project}/>
+          </li>
+        )) }
+      </ul>
+    </Layout>
+  )
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allProjects = await getAllProjects();
+
+  return {
+    props: {
+      allProjects,
+    }
+  }
+};
+
+export default Projects;
