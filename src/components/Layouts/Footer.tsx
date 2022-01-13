54e5fe1da3612148d4d2f777f46c0d3d25013ca5
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-01-13 13:17:00 -0300

    add footer content

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -1,9 +1,18 @@
+import { Contacts } from '@components/Contacts';
+
 import styles from '@styles/components/Layouts/Footer.module.scss';
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footerContainer}>
+      <div className={styles.siteDetails}>
+        <p>Designed <em>(Inspired by others)</em> and developed by Caio Vinícius.</p>
+        <p>
+          Built with <strong>Next.js</strong> and hosted on <strong>Vercel</strong>.
+        </p>
+      </div>
 
+      <Contacts />
     </footer>    
   )
 };
