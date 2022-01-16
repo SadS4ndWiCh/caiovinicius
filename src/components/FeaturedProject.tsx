2f385eec92bd0e8ef21c58a968207af124e85c98
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-01-16 12:18:11 -0300

    render thumb if exist icon

--- src/components/FeaturedProject.tsx
+++ src/components/FeaturedProject.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
-import { IProject } from "@lib/graphcms";
+import { IFeaturedProject } from "@lib/graphcms";
 
 import HandWEBP from '@public/images/hand.webp';
 
 import styles from '@styles/components/FeaturedProjects.module.scss';
 
 interface FeaturedProjectProps {
-  project: IProject;
+  project: IFeaturedProject;
 };
 
 export const FeaturedProject = ({ project }: FeaturedProjectProps) => {
@@ -17,9 +17,11 @@ export const FeaturedProject = ({ project }: FeaturedProjectProps) => {
     <div className={styles.featuredProjectContainer}>
       <div className={styles.content}>
         <div className={styles.header}>
-          <div className={styles.thumb}>
-            <Image src={HandWEBP} alt='icon' />
-          </div>
+          { project.icon && (
+            <div className={styles.thumb}>
+              <Image src={project.icon.url} width={25} height={25} alt='icon' />
+            </div>
+          ) }
           <h3>{ project.name }</h3>
           <span className={styles.createdAt}>{ createdYear }</span>
         </div>
