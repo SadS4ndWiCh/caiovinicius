b8e29aae136a45c5c1d7740c3b7c8b4693b7590e
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-01-16 12:26:51 -0300

    add list posts page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,48 @@
+import type { GetStaticProps, NextPage } from "next";
+
+import * as GraphCMS from "@lib/graphcms";
+
+import { Layout } from "@components/Layouts/Layout";
+import { PostCard } from "@components/PostCard";
+
+import styles from '@styles/pages/Blog/Blog.module.scss';
+
+interface BlogProps {
+  allPosts: GraphCMS.IPostDetails[]
+};
+
+const Blog: NextPage<BlogProps> = ({ allPosts }) => {
+  return (
+    <Layout 
+      seo={{
+        title: 'Blog',
+        description: 'My tech blog'
+      }}
+      className={styles.container}
+    >
+      <h1>Blog</h1>
+
+      <main>
+        <ul className={styles.postsList}>
+          { allPosts.map(post => (
+            <li key={post.slug}>
+              <PostCard post={post} />
+            </li>
+          )) }
+        </ul>
+      </main>
+    </Layout>
+  )
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPosts = await GraphCMS.getAllPosts();
+  
+  return {
+    props: {
+      allPosts
+    }
+  };
+};
+
+export default Blog;
