18141f5f6843d997690798e501ce9db85849dbfe
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-28 12:21:03 -0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'identity': 'linear-gradient(270deg, #D61E1E 0%, #EB27CC 53.13%, #273BEB 100%)',
+        'primary': '#171717',
+        'secundary': '#2E2E2E',
+        'text': '737373',
+      }
+    },
   },
   plugins: [],
 }
