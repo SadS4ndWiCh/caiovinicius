9fb812f3f11b6fb39ea38e1d2babb6bf5ffee975
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-28 13:49:32 -0300

    use Inter font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ const Document = () => {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
