60c92d0c3a10ddb19d35f54ce255480a0e117e66
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-28 12:29:42 -0300

    add primary colors variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,19 @@ module.exports = {
     extend: {
       colors: {
         'identity': 'linear-gradient(270deg, #D61E1E 0%, #EB27CC 53.13%, #273BEB 100%)',
-        'primary': '#171717',
-        'secundary': '#2E2E2E',
-        'text': '737373',
+        'primary': {
+          900: '#171717',
+          800: '#202020',
+          700: '#212121',
+          600: '#232323',
+          500: '#262626',
+          400: '#2D2D2D',
+          300: '#3B3B3B',
+          200: '#575757',
+          100: '#8F8F8F',
+        },
+        'heading': '#FFFFFF',
+        'text': '#737373',
       }
     },
   },
