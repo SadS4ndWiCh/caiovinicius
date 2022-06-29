8cd8a1a5ff1af365194a20b6d270629aee0ef3fe
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-29 19:19:11 -0300

    add responsivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./Icons/Logo";
 
 export const Header = () => {
   return (
-    <header className='pt-12'>
+    <header className='flex items-center justify-center pt-12 md:justify-start'>
       <Logo />
     </header>
   )
