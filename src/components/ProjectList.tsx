0f9e17ab999b08cec06b05ebfca4a9198f7842ff
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-29 20:40:25 -0300

    add projects prop and map all projects

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,10 +1,18 @@
-import { Project } from "./Project";
+import { IProject, Project } from "./Project";
 
-export const ProjectList = () => {
+type Props = {
+  projects: IProject[];
+};
+
+export const ProjectList = ({ projects }: Props) => {
   return (
     <div className='flex flex-col gap-9 mt-9'>
-      <Project />
-      <Project />
+      { projects.map(project => (
+        <Project
+          key={project.id}
+          project={project}
+        />
+      )) }
     </div>
   )
 };
