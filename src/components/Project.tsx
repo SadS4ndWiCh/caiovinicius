facb000762fe430e6c909ca02272e592ca55b170
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-29 12:39:17 -0300

    add gradient border in tags

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpRight } from "phosphor-react";
 import { Link } from "./Link";
 
 export const Project = () => {
@@ -7,9 +8,13 @@ export const Project = () => {
       <div className='flex-1'>
         <header>
           <div>
-            <span className='text-xs px-2 py-[0.125rem] border border-indigo-700 rounded-full text-white'>
-              React
-            </span>
+            <div
+              className='w-fit bg-identity rounded-full px-[0.125rem] py-[0.125rem]'
+            >
+              <span className='block text-xs px-3 py-[0.125rem] rounded-full bg-primary-900 text-white'>
+                React
+              </span>
+            </div>
           </div>
           <h3
             className='text-4xl font-bold text-heading'
@@ -26,15 +31,15 @@ export const Project = () => {
         <footer className='flex items-center justify-between mt-4'>
           <Link
             href='#'
-            className='text-xs px-3 py-1 text-white border border-primary-300 rounded-full'
+            className='text-xs px-3 py-1 text-white border border-primary-300 rounded-full transition-colors hover:bg-primary-300'
           >
             Source Code
           </Link>
           <Link
             href='#'
-            className='text-xs px-3 py-1 text-white border border-primary-300 rounded-full bg-primary-300'
+            className='flex items-center gap-1 text-xs px-3 py-1 text-white border border-primary-300 rounded-full bg-primary-300 transition-colors hover:bg-primary-900'
           >
-            View Demo
+            View Demo <ArrowUpRight size={16} />
           </Link>
         </footer>
       </div>
