35e7624d5b139031dda369ff4e7cfbf39d2fad17
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-30 17:47:13 -0300

    add announcemente component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import '@styles/global.css';
 
 import { ApolloProvider } from '@apollo/client';
 import { client } from '@lib/apollo';
+import { Announcement } from '@components/Announcement';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Announcement message='The website is currently under development.' />
       <Component {...pageProps} />
     </ApolloProvider>
   )
