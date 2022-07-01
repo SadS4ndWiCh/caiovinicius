9a212e7e479f8b43d15c15503c41453162df6d89
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-07-01 20:15:26 -0300

    add animation in introduction section

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,15 +1,46 @@
+import { motion } from 'framer-motion';
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 export const Introduction = () => {
   return (
     <div className='md:max-w-[553px]'>
-      <h1 className='font-extrabold leading-tight text-4xl text-center text-heading md:text-5xl md:text-left'>
+      <motion.h1
+        initial='hidden'
+        animate='visible'
+        variants={variants}
+        transition={{
+          duration: .5
+        }}
+        className='font-extrabold leading-tight text-4xl text-center text-heading md:text-5xl md:text-left'
+      >
         I&apos;m Caio Vinícius,<br />
         an 18 year old Brazilian <br />
         web developer.
-      </h1>
-      <p className='mt-3 text-center leading-relaxed md:text-left'>
+      </motion.h1>
+      
+      <motion.p
+        initial='hidden'
+        animate='visible'
+        variants={variants}
+        transition={{
+          delay: .25,
+          duration: .5
+        }}
+        className='mt-3 text-center leading-relaxed md:text-left'
+      >
       Hey! I&apos;ve been studying web development for just over a year. I&apos;m currently doing an internship at Compass.UOL as a Product 
       Designer, which has been very good for me to improve on UI/UX issues.
-      </p>
+      </motion.p>
     </div>
   )
 };
