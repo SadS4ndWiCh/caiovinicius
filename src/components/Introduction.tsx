f5f7a7970975be331f538867286a3fd686307cce
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-07-23 16:58:26 -0300

    fix acessibility introduction

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -21,7 +21,7 @@ export const Introduction = () => {
         transition={{
           duration: .5
         }}
-        className='font-extrabold leading-tight text-4xl text-center text-heading md:text-5xl md:text-left'
+        className='font-extrabold leading-tight text-3xl text-center text-heading md:text-5xl md:text-left'
       >
         I&apos;m Caio Vinícius,<br />
         an 18 year old Brazilian <br />
@@ -36,9 +36,10 @@ export const Introduction = () => {
           delay: .25,
           duration: .5
         }}
-        className='mt-3 text-center leading-relaxed md:text-left'
+        className='mt-3 leading-relaxed md:text-left'
       >
-      Hey! I&apos;ve been studying web development for just over a year. I&apos;m currently doing an internship at Compass.UOL as a Product 
+      Hey! I&apos;ve been studying web development for just over a year. I&apos;m currently doing an internship at {' '}
+      <a href="https://compass.uol" className='underline underline-offset-2 text-zinc-300 hover:text-zinc-100'>Compass.UOL</a> as a Product 
       Designer, which has been very good for me to improve on UI/UX issues.
       </motion.p>
     </div>
