1c79510d1296a7d27c7f563c2dd711cdd50a2a4e
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-11 11:15:40 -0300

    wrapped with section tag and include socials component

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,4 +1,10 @@
 import { motion } from 'framer-motion';
+import { Socials } from './Socials';
+
+type Props = {
+  heading: string;
+  summary: string;
+}
 
 const variants = {
   hidden: {
@@ -11,37 +17,41 @@ const variants = {
   },
 };
 
-export const Introduction = () => {
+export const Introduction = ({ heading, summary }: Props) => {
   return (
-    <div className='md:max-w-[553px]'>
-      <motion.h1
-        initial='hidden'
-        animate='visible'
-        variants={variants}
-        transition={{
-          duration: .5
-        }}
-        className='font-extrabold leading-tight text-3xl text-center text-heading md:text-5xl md:text-left'
-      >
-        I&apos;m Caio Vinícius,<br />
-        an 18 year old Brazilian <br />
-        web developer.
-      </motion.h1>
-      
-      <motion.p
-        initial='hidden'
-        animate='visible'
-        variants={variants}
-        transition={{
-          delay: .25,
-          duration: .5
-        }}
-        className='mt-3 leading-relaxed md:text-left'
-      >
-      Hey! I&apos;ve been studying web development for just over a year. I&apos;m currently doing an internship at {' '}
-      <a href="https://compass.uol" className='underline underline-offset-2 text-zinc-300 hover:text-zinc-100'>Compass.UOL</a> as a Product 
-      Designer, which has been very good for me to improve on UI/UX issues.
-      </motion.p>
-    </div>
+    <section
+      id='introduction'
+      className='block mt-20 md:flex md:justify-between md:flex-row'
+    >
+      <div className='md:max-w-[553px]'>
+        <motion.h1
+          initial='hidden'
+          animate='visible'
+          variants={variants}
+          transition={{
+            duration: .5
+          }}
+          className='font-extrabold leading-tight text-3xl text-center text-heading md:text-5xl md:text-left'
+        >
+          { heading }
+        </motion.h1>
+        
+        <motion.p
+          initial='hidden'
+          animate='visible'
+          variants={variants}
+          transition={{
+            delay: .25,
+            duration: .5
+          }}
+          className='mt-3 leading-relaxed md:text-left'
+        >
+          { summary }
+        </motion.p>
+      </div>
+
+      <Socials />
+    </section>
+    
   )
 };
