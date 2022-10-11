790a44f1e7d14861566e420fab99c17f16932d1c
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-11 11:12:16 -0300

    add nav links to sections

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
 import { Logo } from "./Icons/Logo";
 
+const navLinks = [
+  { label: 'Introduction', destination: '#introduction' },
+  { label: 'Projects', destination: '#projects' },
+  { label: 'About', destination: '#about' },
+]
+
 export const Header = () => {
   return (
-    <header className='flex items-center justify-center pt-12 md:justify-start'>
+    <header className='sticky top-2 left-0 right-0 z-10 flex items-center justify-center px-6 py-6 backdrop-saturate-[180%] backdrop-blur-[20px] rounded-lg md:justify-between'>
       <Logo />
+
+      <ul className='flex items-center gap-2'>
+        { navLinks.map(link => (
+          <li key={link.label}>
+            <a href={link.destination} className='flex p-2 hover:text-white'>
+              { link.label }
+            </a>
+          </li>
+        )) }
+      </ul> 
     </header>
   )
 };
