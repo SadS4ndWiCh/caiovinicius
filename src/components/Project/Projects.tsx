0156ec216a451ddfd90f5ef3487354d9d4ab0522
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-11 11:14:58 -0300

    wrapped with section tag and moved to project folder

---
+++ src/components/Project/Projects.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import { IProject, Project } from ".";
+
+type Props = {
+  projects: IProject[];
+};
+
+export const Projects = ({ projects }: Props) => {
+  return (
+    <section id='projects' className='mt-16 md:mt-24'>
+      <motion.h2
+        initial={{ opacity: 0, x: -200 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className='text-5xl font-extrabold text-heading'
+      >
+        Projetcs
+      </motion.h2>
+
+      <div className='flex flex-col gap-9 mt-9'>
+        { projects.map(project => (
+          <Project
+            key={project.id}
+            project={project}
+          />
+        )) }
+      </div>     
+    </section>
+    
+  )
+};
