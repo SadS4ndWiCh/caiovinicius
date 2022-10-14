a57a4069686bce8387d6e1d081c368f064ccfaff
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-14 15:58:47 -0300

    implements text and heading components

--- src/components/Project/Projects.tsx
+++ src/components/Project/Projects.tsx
@@ -1,3 +1,4 @@
+import { Heading } from "@components/Heading";
 import { motion } from "framer-motion";
 import { IProject, Project } from "./";
 
@@ -8,14 +9,15 @@ type Props = {
 export const Projects = ({ projects }: Props) => {
   return (
     <section id='projects' className='mt-16 md:mt-24'>
-      <motion.h2
-        initial={{ opacity: 0, x: -200 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-        className='text-5xl font-extrabold text-heading'
-      >
-        Projetcs
-      </motion.h2>
+      <Heading asChild size='lg'>
+        <motion.h2
+          initial={{ opacity: 0, x: -200 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Projetcs
+        </motion.h2>
+      </Heading>
 
       <div className='flex flex-col gap-9 mt-9'>
         { projects.map(project => (
--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -4,6 +4,8 @@ import { useInViewAnimation } from '@hooks/useInViewAnimation';
 
 import { FiArrowUpRight } from 'react-icons/fi';
 import { FaGithub } from "react-icons/fa";
+import { Text } from "@components/Text";
+import { Heading } from "@components/Heading";
 
 export interface IProject {
   id: string;
@@ -72,30 +74,35 @@ export const Project = ({ project }: Props) => {
           <div className='flex items-center gap-2'>
             { project.tags.map(tag => (
               <div key={tag} className='w-fit bg-identity rounded-full px-[0.125rem] py-[0.125rem]'>
-                <span
+                <Text
+                  size='sm'
                   className='block text-[10px] px-3 py-[0.125rem] rounded-full bg-primary-900 text-white uppercase tracking-widest'
                 >
                   { tag }
-                </span>
+                </Text>
               </div>
             )) }
           </div>
-          <h3
-            className='mt-1 text-4xl font-bold text-heading'
-          >
-            { project.name }
-          </h3>
+          <Heading asChild size='md'>
+            <h3
+              className='mt-1'
+            >
+              { project.name }
+            </h3>
+          </Heading>
         </motion.header>
         
-        <motion.p
-          initial='hidden'
-          animate={controls}
-          variants={variants}
-          transition={{ delay: .5, duration: 1 }}
-          className='mt-4'
-        >
-          { project.description }
-        </motion.p>
+        <Text asChild>
+          <motion.p
+            initial='hidden'
+            animate={controls}
+            variants={variants}
+            transition={{ delay: .5, duration: 1 }}
+            className='mt-4'
+          >
+            { project.description }
+          </motion.p>
+        </Text>
         
         <motion.footer
           initial='hidden'
@@ -104,23 +111,27 @@ export const Project = ({ project }: Props) => {
           transition={{ delay: .75, duration: 1 }}
           className='flex items-center justify-between mt-4'
         >
-          <a
-            href={project.sourceCode}
-            target='_blank'
-            rel='noreferrer'
-            className='flex items-center gap-2 text-xs px-3 py-2 text-white border border-primary-300 rounded-full transition-colors hover:bg-primary-300'
-          >
-            Source Code <FaGithub size={16} />
-          </a>
-          { project.demo && (
+          <Text asChild size='sm'>
             <a
-              href={project.demo}
+              href={project.sourceCode}
               target='_blank'
               rel='noreferrer'
-              className='flex items-center gap-1 text-xs px-3 py-2 text-white border border-primary-300 rounded-full bg-primary-300 transition-colors hover:bg-primary-900'
+              className='flex items-center gap-2 px-3 py-2 text-white border border-primary-300 rounded-full transition-colors hover:bg-primary-300'
             >
-              View Demo <FiArrowUpRight size={16} />
+              Source Code <FaGithub size={16} />
             </a>
+          </Text>
+          { project.demo && (
+            <Text asChild size='sm'>
+              <a
+                href={project.demo}
+                target='_blank'
+                rel='noreferrer'
+                className='flex items-center gap-1 px-3 py-2 text-white border border-primary-300 rounded-full bg-primary-300 transition-colors hover:bg-primary-900'
+              >
+                View Demo <FiArrowUpRight size={16} />
+              </a>
+            </Text>
           ) }
         </motion.footer>
       </div>
