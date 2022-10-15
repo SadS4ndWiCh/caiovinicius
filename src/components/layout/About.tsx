e2945b1e2d1a6585e1f8226fc34d2c40fd3242c2
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-15 16:44:03 -0300

    add animation in about section

--- src/components/layout/About.tsx
+++ src/components/layout/About.tsx
@@ -1,3 +1,7 @@
+import { motion } from "framer-motion";
+
+import { useInViewAnimation } from "@hooks/useInViewAnimation";
+
 import { Heading } from "@components/Heading";
 import { Text } from "@components/Text";
 
@@ -5,14 +9,57 @@ type Props = {
 	about: string;
 }
 
+const variants = {
+  hidden: {
+    opacity: 0,
+    y: -30,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 export const About = ({ about }: Props) => {
+	const { ref, controls } = useInViewAnimation({
+		animationVariantName: 'visible',
+		options: {
+			threshold: .1
+		}
+	});
+
 	return (
-    <section id='about' className="mt-16 md:mt-24">
-			<Heading size='lg'>
-				About
+    <section
+			id='about'
+			className="mt-16 md:mt-24"
+		>
+			<Heading asChild size='lg'>
+				<motion.h2
+					ref={ref}
+					initial='hidden'
+					animate={controls}
+					variants={variants}
+					transition={{
+						duration: 1,
+					}}
+				>
+					About
+				</motion.h2>
 			</Heading>
 			<Text asChild>
-				<p className="mt-3">{ about }</p>
+				<motion.p
+					ref={ref}
+					initial='hidden'
+					animate={controls}
+					variants={variants}
+					transition={{
+						delay: .25,
+						duration: 1,
+					}}
+					className="mt-3"
+				>
+					{ about }
+				</motion.p>
 			</Text>
 		</section>
 	)
