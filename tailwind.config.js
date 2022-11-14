d734fb8802c00467ea694c836c11eba5ea1889b7
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-14 20:34:03 -0300

    add shadows style

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,10 @@ module.exports = {
         'grid': 'url(/images/grid.png)',
         'identity': 'linear-gradient(270deg, #D61E1E 0%, #EB27CC 53.13%, #273BEB 100%)'
       },
+      boxShadow: {
+        'gray-blur': '0 0 20px #3f3f46ab',
+        identity: '0 0 10px #EB27CC',
+      },
       colors: {
         'primary': {
           900: '#171717',
