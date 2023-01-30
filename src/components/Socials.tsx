ca4de5deb147f573c7a98c1f25a67762bbf18e1d
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-30 15:20:37 -0300

    refactor: change social list component

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,22 +1,22 @@
 import { motion } from 'framer-motion';
 
-import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import { Icons } from './Icons';
 
 const SOCIALS_LINKS = [
   {
-    id: 'social-twitter',
+    title: 'Twitter',
     href: 'https://twitter.com/SadSAndWiCh_',
-    icon: <FaTwitter className='text-primary-900' size={20} />
+    Icon: Icons.twitter
   },
   {
-    id: 'social-github',
+    title: 'Github',
     href: 'https://github.com/SadS4ndWiCh',
-    icon: <FaGithub className='text-primary-900' size={20} />
+    Icon: Icons.github
   },
   {
-    id: 'social-linkedin',
+    title: 'Linkedin',
     href: 'https://www.linkedin.com/in/caiocamargo007/',
-    icon: <FaLinkedinIn className='text-primary-900' size={20} />
+    Icon: Icons.linkedin
   },
 ]
 
@@ -27,7 +27,7 @@ const socialVariants = {
   visible: (i: number) => ({
     opacity: 1,
     transition: {
-      delay: i * 0.15,
+      delay: i * 0.15 + .5,
     },
   })
 };
@@ -35,11 +35,11 @@ const socialVariants = {
 export const Socials = () => {
   return (
     <div
-      className='flex items-center gap-1 w-full mt-10 sm:flex-col sm:max-w-none sm:w-fit sm:mt-0'
+      className='flex items-center gap-6 mt-12'
     >
       { SOCIALS_LINKS.map((social, i) => (
         <motion.a
-          key={social.id}
+          key={social.title}
           custom={i}
           initial='hidden'
           animate='visible'
@@ -47,18 +47,13 @@ export const Socials = () => {
           href={social.href}
           target='_blank'
           rel='noreferrer'
-          className='shrink-0 flex items-center justify-center w-[38px] h-[38px] bg-white transition-colors hover:bg-primary-100'
+          className='group flex items-center gap-4 text-zinc-300 hover:text-white'
         >
-          { social.icon }
+          <social.Icon className='w-5 h-5 fill-zinc-300 group-hover:fill-white' />
+          { social.title }
+          <Icons.arrow className='w-5 h-5 -rotate-45' />
         </motion.a>
       )) }
-
-      <motion.div
-        initial={{ height: 0 }}
-        animate={{ height: '100%' }}
-        transition={{ duration: 1 }}
-        className='w-full h-[1px] bg-primary-100 sm:w-[1px] sm:h-full'
-      />
     </div>
   )
 };
