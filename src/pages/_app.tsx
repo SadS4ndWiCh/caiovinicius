5e5a52ae4d50f4f05c89408d91cd3157c7634c4e
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-02-01 22:23:29 -0300

    chore: remove unnecessary content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,34 +2,9 @@ import type { AppProps } from 'next/app';
 
 import '@styles/global.css';
 
-import { ApolloProvider } from '@apollo/client';
-import { client } from '@lib/apollo';
-
-import { Analitycs } from '@components/Analitycs';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      // @ts-ignore
-      gtag.pageView(url);
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    }
-  });
-
   return (
-    <ApolloProvider client={client}>
-      {/* <Announcement message='The website is currently under development.' /> */}
-      <Component {...pageProps} />
-      <Analitycs />
-    </ApolloProvider>
+    <Component {...pageProps} />
   )
 }
 
