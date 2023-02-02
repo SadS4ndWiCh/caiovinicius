bc9607ff1989f1f0b6bc6b953b096fcdcbf68fd9
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-02-01 22:05:11 -0300

    style: adjust imports brackets

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
