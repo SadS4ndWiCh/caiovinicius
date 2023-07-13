26bfe5e1beedf869e5196352e69d682066a6c3ad
sads4ndwich <caiocamargo007@gmail.com>
2023-07-13 15:00:20 -0300

    chore: remove all old components

--- src/components/layout/Background.tsx
+++
@@ -1,8 +0,0 @@
-export const Background = () => {
-	return (
-		<div>
-			<div className="absolute -z-10 -top-32 left-10 w-96 h-96 bg-fuchsia-600 rounded-full blur-[170px]" />
-			<div className="absolute -z-10 top-40 right-10 w-[400px] h-[400px] bg-cyan-400 rounded-full blur-[200px]" />
-		</div>
-	)
-}
