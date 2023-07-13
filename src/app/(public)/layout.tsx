beb68ce8afad2bb4c213b3b1dac6c79c27944f0d
sads4ndwich <caiocamargo007@gmail.com>
2023-07-13 15:00:37 -0300

    chore: remove old content

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,17 +1,21 @@
-import '@styles/global.css';
-import type { Metadata } from 'next';
-import type { ReactNode } from 'react';
+import type { ReactNode } from 'react'
 
-import { Roboto } from 'next/font/google';
-import { Footer } from '~/components/layout/Footer';
-import { Header } from '~/components/layout/Header';
+import type { Metadata } from 'next'
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'], variable: '--font-sans' });
+import { Roboto } from 'next/font/google'
+
+import '@styles/global.css'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
   title: {
     default: 'Caio Vinícius',
-    template: '%s | Caio Vinícius'
+    template: '%s | Caio Vinícius',
   },
   creator: 'Caio Vinícius',
   icons: {
@@ -25,19 +29,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <head />
-      <body className={roboto.className}>
-        <div className='flex flex-col min-h-screen relative before:sticky before:top-0 before:left-0 before:right-0 before:content-[""] before:z-50 before:block before:w-full before:h-1 before:bg-identity'>
-          <div className='h-full lg:px-0'>
-            <Header />
-
-            <main>
-              {children}
-            </main>
-
-            <Footer />
-          </div>
-        </div>
-      </body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,24 +1,20 @@
-import { cachedClient } from "src/sanity/lib/client";
-import { aboutQuery, projectsQuery } from "src/sanity/lib/queries";
-import { IProject } from "src/types";
-import { About } from "~/components/layout/About";
-import { Introduction } from "~/components/layout/Introduction";
-import { Projects } from "~/components/layout/Projects";
+import { cachedClient } from 'src/sanity/lib/client'
+import { aboutQuery, projectsQuery } from 'src/sanity/lib/queries'
+
+import { IProject } from 'src/types'
 
 type About = {
-  summary: string;
-  aboutMe: string;
+  summary: string
+  aboutMe: string
 }
 
 export default async function Home() {
-  const projects: IProject[] = await cachedClient(projectsQuery);
-  const about: About = await cachedClient(aboutQuery);
+  const projects: IProject[] = await cachedClient(projectsQuery)
+  const about: About = await cachedClient(aboutQuery)
 
   return (
-    <>
-      <Introduction summary={about.summary} />
-      <Projects projects={projects} />
-      <About about={about.aboutMe} />
-    </>
+    <main>
+      <h1>Hello WOrld</h1>
+    </main>
   )
 }
