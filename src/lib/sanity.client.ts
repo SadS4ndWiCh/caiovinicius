78272bb792bb5321e4f26f662f625fa3357f3cb1
sads4ndwich <caiocamargo007@gmail.com>
2023-07-13 12:17:07 -0300

    feat(sanity): export cached client

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,12 +1,15 @@
 import { createClient } from "next-sanity";
+import { cache } from "react";
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION as string;
 
-export const client = createClient({
-	projectId,
-	dataset,
-	apiVersion,
-	useCdn: process.env.NODE_ENV === 'production'
+const _client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: process.env.NODE_ENV === 'production'
 });
+
+export const client = cache(_client.fetch.bind(_client));
