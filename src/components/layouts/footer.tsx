6bdad6da67c46e9924810379dfa5d19173fb3f84
sads4ndwich <caiocamargo007@gmail.com>
2023-07-14 15:27:08 -0300

    feat: include link and built by

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,14 +1,62 @@
+import Link from 'next/link'
+
+import { links, siteConfig } from '~/config/site'
+
+import { cn } from '~/lib/utils'
+
 import { Spotify } from '../spotify'
+import { buttonVariants } from '../ui/button'
 
 export function Footer() {
   return (
     <footer>
-      <div className="max-w-3xl mx-auto flex flex-col items-center justify-between p-4 gap-4 md:flex-row">
-        <Spotify />
+      <div className="max-w-3xl mx-auto flex flex-col p-4 gap-4">
+        <div className="flex flex-col items-center justify-between gap-6 md:flex-row md:items-start">
+          <Spotify />
 
-        <p className="text-slate-400 text-sm flex-shrink-0">
-          All rights reserved © Caio Vinícius 2023
-        </p>
+          <nav className="flex justify-end gap-4">
+            {siteConfig.footerNav.map((nav) => (
+              <div key={nav.title}>
+                <h4 className="text-center text-white font-bold text-sm">
+                  {nav.title}
+                </h4>
+
+                <ul>
+                  {nav.items.map((link) => (
+                    <li key={link.title} className="text-center">
+                      <Link
+                        href={link.href}
+                        rel={link.external ? 'noreferrer' : undefined}
+                        target={link.external ? '_blank' : undefined}
+                        className={cn(
+                          buttonVariants({ variant: 'link', size: 'sm' }),
+                          'text-slate-300 text-sm',
+                        )}
+                      >
+                        {link.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </nav>
+        </div>
+
+        <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+          <p className="text-xs text-slate-400">
+            Built by{' '}
+            <a
+              href={links.github}
+              className="font-bold text-slate-300 underline underline-offset-4"
+            >
+              Caio Vinícius
+            </a>
+          </p>
+          <p className="text-slate-400 text-xs text-center flex-shrink-0 md:text-left">
+            All rights reserved © Caio Vinícius 2023
+          </p>
+        </div>
       </div>
     </footer>
   )
