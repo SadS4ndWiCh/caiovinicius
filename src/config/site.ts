cd00a4c60c98b14e3dccf6ec6ea2dc84f9ce010c
sads4ndwich <caiocamargo007@gmail.com>
2023-07-14 15:26:28 -0300

    feat: added site config data

---
+++ src/config/site.ts
@@ -0,0 +1,44 @@
+export const links = {
+  twitter: 'https://twitter.com/SadSAndWiCh_',
+  linkedin: 'https://www.linkedin.com/in/caiocamargo007/',
+  github: 'https://github.com/SadS4ndWiCh/caiovinicius',
+  githubAccount: 'https://github.com/SadS4ndWiCh',
+}
+
+export const siteConfig = {
+  name: 'Caio Vinícius',
+  description:
+    'Web developer with product design experience and passion for front-end. Proficient in Python, Javascript and C#. Studying Analisys and System Development. Continuous self-improvement and internship experience. Dedicated and driven.',
+  footerNav: [
+    {
+      title: 'Credits',
+      items: [
+        {
+          title: 'shadcn/ui',
+          href: 'https://ui.shadcn.com',
+          external: true,
+        },
+      ],
+    },
+    {
+      title: 'Socials',
+      items: [
+        {
+          title: 'Twitter',
+          href: links.twitter,
+          external: true,
+        },
+        {
+          title: 'Linkedin',
+          href: links.linkedin,
+          external: true,
+        },
+        {
+          title: 'GitHub',
+          href: links.github,
+          external: true,
+        },
+      ],
+    },
+  ],
+}
