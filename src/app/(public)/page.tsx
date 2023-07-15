becca8473c22ec02eccca52c6e224e50be51ddab
sads4ndwich <caiocamargo007@gmail.com>
2023-07-15 15:17:45 -0300

    feat: include bg grid

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -6,7 +6,7 @@ import { Separator } from '~/components/ui/separator'
 export default async function Home() {
   return (
     <div className="flex flex-col max-w-3xl mx-auto p-4">
-      <section className="mt-4 space-y-2">
+      <section className="mt-4 space-y-2 before:absolute before:top-0 before:left-0 before:right-0 before:h-[50vh] before:-z-20 before:bg-grid-slate-900/60 after:absolute after:top-0 after:left-0 after:right-0 after:h-[50vh] after:bg-gradient-to-b after:from-black/0 after:to-80% after:to-slate-950 after:-z-10">
         <h1 className="text-white font-bold text-xl">Caio Vinícius</h1>
         <p className="text-slate-300 leading-relaxed">
           Web developer with product design experience and passion for
