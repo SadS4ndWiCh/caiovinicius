3d96c348b47a87130fa161bd567cb94af4190aa7
sads4ndwich <caiocamargo007@gmail.com>
2023-07-25 15:07:21 -0300

    fix: wrong req type and change sanity signature validation

--- src/app/api/sanity/revalidate/route.ts
+++ src/app/api/sanity/revalidate/route.ts
@@ -1,14 +1,15 @@
-import { NextApiRequest } from 'next'
-
 import { revalidatePath } from 'next/cache'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-import { isValidRequest } from '@sanity/webhook'
+import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook'
 
 const webhookSecrect = process.env.SANITY_WEBHOOK_SECRET as string
 
-export async function POST(req: NextApiRequest) {
-  if (!isValidRequest(req, webhookSecrect))
+export async function POST(req: NextRequest) {
+  const signature = req.headers.get(SIGNATURE_HEADER_NAME)
+  const body = await req.json()
+
+  if (!signature || !isValidSignature(body, signature, webhookSecrect))
     return NextResponse.json(null, { status: 401 })
 
   try {
