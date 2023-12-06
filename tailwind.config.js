a9659bcce18ae61bba448ccc16529219af5a788f
sads4ndwich <caiocamargo007@gmail.com>
2023-12-06 09:40:11 -0300

    refactor: remove bg grid custom plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
-import svgToDataUri from 'mini-svg-data-uri'
 import { fontFamily } from 'tailwindcss/defaultTheme'
-
-import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette'
 
 /** @type {import('tailwindcss').Config} */
 export const darkMode = ['class']
@@ -78,19 +75,5 @@ export const theme = {
       'accordion-up': 'accordion-up 0.2s ease-out',
     },
   },
+  plugins: [require('tailwindcss-animate')]
 }
-export const plugins = [
-  require('tailwindcss-animate'),
-  function ({ matchUtilities, theme }) {
-    matchUtilities(
-      {
-        'bg-grid': (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
-          )}")`,
-        }),
-      },
-      { values: flattenColorPalette(theme('backgroundColor')), type: 'color' },
-    )
-  },
-]
