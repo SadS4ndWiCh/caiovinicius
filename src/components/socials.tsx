787d44a5af42863be6092aa2369da36eeac13283
sads4ndwich <caiocamargo007@gmail.com>
2023-12-12 16:55:45 -0300

    style: use only social title instead icons

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,36 +1,23 @@
-import { Github, Linkedin, Twitter } from 'lucide-react'
-
 import { cn } from '~/lib/utils'
 
 import { buttonVariants } from './ui/button'
+import { siteConfig } from '~/config/site'
 
 export function Socials() {
   return (
-    <div className="space-x-2">
-      <a
-        href="https://twitter.com/SadSAndWiCh_"
-        rel="noreferrer"
-        target="_blank"
-        className={cn(buttonVariants({ size: 'icon', variant: 'ghost' }))}
-      >
-        <Twitter className="w-4 h-4" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/caiocamargo007/"
-        rel="noreferrer"
-        target="_blank"
-        className={cn(buttonVariants({ size: 'icon', variant: 'ghost' }))}
-      >
-        <Linkedin className="w-4 h-4" />
-      </a>
-      <a
-        href="https://github.com/SadS4ndWiCh"
-        rel="noreferrer"
-        target="_blank"
-        className={cn(buttonVariants({ size: 'icon', variant: 'ghost' }))}
-      >
-        <Github className="w-4 h-4" />
-      </a>
-    </div>
+    <ul className="flex">
+      {siteConfig.socials.map(social => (
+        <li key={social.title}>
+          <a
+            href={social.href}
+            rel="noreferrer"
+            target="_blank"
+            className={cn(buttonVariants({ variant: 'link' }), 'text-muted-foreground')}
+          >
+            {social.title}
+          </a>
+        </li>
+      ))}
+    </ul>
   )
 }
