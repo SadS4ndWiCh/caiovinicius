02a4a379437d9d1f37d940c058974b8caa63892b
sads4ndwich <caiocamargo007@gmail.com>
2023-12-12 16:58:28 -0300

    style: include socials in footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,7 +1,11 @@
+import { Socials } from "../socials";
+
 export function Footer() {
   return (
-    <footer className="max-w-3xl w-full mx-auto p-4">
+    <footer className="flex items-center justify-between max-w-3xl w-full mx-auto p-4">
       <span className="text-sm text-muted-foreground">&copy; Caio Vinícius</span>
+
+      <Socials />
     </footer>
   )
 }
