09c33297de30ac29463332330db44acc54711c99
SadS4ndWiCh <caiocamargo007@gmail.com>
2025-01-01 18:24:16 -0300

    chore: remove projects collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,1 @@
-import { defineCollection, z } from "astro:content";
-
-const projectsCollection = defineCollection({
-    type: "data",
-    schema: z.object({
-        slug: z.string(),
-        name: z.string(),
-        description: z.string(),
-        sourceCode: z.string().url(),
-    }),
-});
-
-export const collections = {
-    projects: projectsCollection,
-};
+export const collections = {};
