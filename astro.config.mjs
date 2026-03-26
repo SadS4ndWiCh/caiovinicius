5275bbf87a39cbe3ebf157daa83e1892c3896b7c
SadS4ndWiCh <hi@caiovinicius.xyz>
2026-03-26 06:30:41 -0300

    feat: upgrade tailwind usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true
-		}
-	}),
-	integrations: [tailwind({ applyBaseStyles: false })],
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "pt-br"]
-	},
+    output: 'static',
+
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true
+        }
+    }),
+
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "pt-br"]
+    },
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
